38aea3eaab80f349826c91879d0e418c5dc8158a
mike castleman <m@mlcastle.net>
2017-05-14 17:56:57 -0400

    certs-to-acm first draft

---
+++ handler.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const AWS = require('aws-sdk'),
+  pem = require('pem'),
+  promisify = require('es6-promisify');
+
+const region = 'us-east-1',
+  prefix = process.env['STAGE'] + '/';
+
+const readCertificateInfo = promisify(pem.readCertificateInfo);
+
+module.exports.main = (event, context, callback) => {
+  const s3 = new AWS.S3({region: region});
+  const acm = new AWS.ACM({region: region});
+
+  const existingCerts = acm.listCertificates({MaxItems: 1000}).promise().then(resp => resp.CertificateSummaryList);
+
+  const existingCert = (cert) =>
+    existingCerts.then(ec => readCertificateInfo(cert).then(certinfo => {
+      const found = ec.find(c => c.DomainName === certinfo.commonName);
+      return found ? found.CertificateArn : null;
+    }));
+
+  const handleCert = (bucket, key) => {
+    return s3.getObject({Bucket: bucket, Key: key}).promise()
+    .then(obj => JSON.parse(obj.Body))
+    .then(cert => existingCert(cert.cert).then(arn => {
+      return acm.importCertificate({
+        CertificateArn: arn,
+        Certificate: cert.cert,
+        CertificateChain: cert.issuerCert,
+        PrivateKey: cert.key.privateKeyPem
+      }).promise();
+    }));
+  };
+
+  console.log(JSON.stringify(event));
+  const records = event.Records.map(r => JSON.parse(r.Sns.Message).Records);
+  Promise.all([].concat(...records)
+    .filter(r => r.s3.object.key.startsWith(prefix))
+    .map(r => handleCert(r.s3.bucket.name, r.s3.object.key)))
+  .then(success => callback(null, success), e => callback(e));
+};
