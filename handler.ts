57abae06dec654711048e4ab20ebc3f09b646af0
mike castleman <m@mlcastle.net>
2021-02-23 17:13:24 -0500

    a bit fancier

--- handler.ts
+++ handler.ts
@@ -1,24 +1,30 @@
-import { S3 } from '@aws-sdk/client-s3';
-import { ACM, CertificateSummary } from '@aws-sdk/client-acm';
+import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import {
+  ACMClient,
+  CertificateSummary,
+  ImportCertificateCommand,
+  paginateListCertificates,
+} from '@aws-sdk/client-acm';
 import getStream from 'get-stream';
-import * as pem from 'pem';
+import { readCertificateInfo, CertificateSubjectReadResult } from 'pem';
 import { promisify } from 'util';
 
 const region = process.env['REGION'];
 const prefix = process.env['STAGE'] + '/';
 
-const readCertificateInfo = promisify<string, pem.CertificateSubjectReadResult>(
-  pem.readCertificateInfo
-);
+const asyncReadCertificateInfo = promisify<
+  string,
+  CertificateSubjectReadResult
+>(readCertificateInfo);
 
-const s3 = new S3({ region });
-const acm = new ACM({ region });
+const s3 = new S3Client({ region });
+const acm = new ACMClient({ region });
 
 const existingCert = async (
   cert: string,
   existingCerts: CertificateSummary[]
 ): Promise<string | undefined> => {
-  const certInfo = await readCertificateInfo(cert);
+  const certInfo = await asyncReadCertificateInfo(cert);
   return existingCerts.find((c) => c.DomainName === certInfo.commonName)
     ?.CertificateArn;
 };
@@ -28,24 +34,32 @@ const handleCert = async (
   Key: string,
   existingCerts: CertificateSummary[]
 ) => {
-  const { Body } = await s3.getObject({ Bucket, Key });
+  const { Body } = await s3.send(new GetObjectCommand({ Bucket, Key }));
   const certJson = await getStream(Body);
   const cert = JSON.parse(certJson);
   const arn = await existingCert(cert.cert, existingCerts);
-  return acm.importCertificate({
-    CertificateArn: arn,
-    Certificate: cert.cert,
-    CertificateChain: cert.issuerCert,
-    PrivateKey: cert.key.privateKeyPem,
-  });
+  return acm.send(
+    new ImportCertificateCommand({
+      CertificateArn: arn,
+      Certificate: cert.cert,
+      CertificateChain: cert.issuerCert,
+      PrivateKey: cert.key.privateKeyPem,
+    })
+  );
+};
+
+const getAllCerts = async (): Promise<CertificateSummary[]> => {
+  const certs: CertificateSummary[][] = [];
+  for await (const page of paginateListCertificates({ client: acm }, {})) {
+    certs.push(page.CertificateSummaryList ?? []);
+  }
+  return certs.flat();
 };
 
 export const main: AWSLambda.SNSHandler = async (event) => {
-  const existingCerts = await acm
-    .listCertificates({ MaxItems: 1000 })
-    .then((resp) => resp.CertificateSummaryList);
-
   console.log(JSON.stringify(event));
+
+  const existingCerts = await getAllCerts();
 
   const records = event.Records.map(
     (r) => JSON.parse(r.Sns.Message).Records
@@ -54,6 +68,6 @@ export const main: AWSLambda.SNSHandler = async (event) => {
   await Promise.all(
     records
       .filter((r) => r.s3.object.key.startsWith(prefix))
-      .map((r) => handleCert(r.s3.bucket.name, r.s3.object.key, existingCerts ?? []))
+      .map((r) => handleCert(r.s3.bucket.name, r.s3.object.key, existingCerts))
   );
 };
